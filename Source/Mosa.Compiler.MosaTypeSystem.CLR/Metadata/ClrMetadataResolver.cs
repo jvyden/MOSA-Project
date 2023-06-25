b1dd9a8f05d1a2d223be12146d1903bf0feb80ec
jvyden <jvyden@jvyden.xyz>
2023-06-24 21:35:27 -0400

    :frowning2:

--- Source/Mosa.Compiler.MosaTypeSystem.CLR/Metadata/ClrMetadataResolver.cs
+++ Source/Mosa.Compiler.MosaTypeSystem.CLR/Metadata/ClrMetadataResolver.cs
@@ -101,7 +101,7 @@ internal class ClrMetadataResolver
 					}
 				case MosaMethod method:
 					{
-						if (method.DeclaringType!.Namespace == "System.Runtime.CompilerServices" && method.DeclaringType!.Name == "NullableAttribute")
+						if (method.DeclaringType!.Namespace == "System.Runtime.CompilerServices" && method.DeclaringType!.Name.StartsWith("Nullable"))
 						{
 							Console.WriteLine($"{method.DeclaringType!.Namespace} {method.DeclaringType!.Name}::{method.Name} - RUN FOR YOUR LIVES");
 							continue;
