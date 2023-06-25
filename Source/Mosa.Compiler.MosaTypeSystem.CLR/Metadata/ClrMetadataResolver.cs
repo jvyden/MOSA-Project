2918152eb2966de3c4821cdba79622b815526e86
jvyden <jvyden@jvyden.xyz>
2023-06-24 21:21:41 -0400

    Revert "NULLABLES? IN MY HOUSEHOLD? NEVER."
    This reverts commit bf28c395a520b61c1dc5900164a1510e4ff3b4f5.

--- Source/Mosa.Compiler.MosaTypeSystem.CLR/Metadata/ClrMetadataResolver.cs
+++ Source/Mosa.Compiler.MosaTypeSystem.CLR/Metadata/ClrMetadataResolver.cs
@@ -101,9 +101,9 @@ internal class ClrMetadataResolver
 					}
 				case MosaMethod method:
 					{
-						if (method.DeclaringType!.Namespace.StartsWith("System.Runtime.CompilerServices"))
+						if (method.DeclaringType.Namespace.StartsWith("System.Runtime.CompilerServices"))
 						{
-							Console.WriteLine($"{method.DeclaringType!.Namespace} {method.DeclaringType!.Name}::{method.Name} - RUN FOR YOUR LIVES");
+							Console.WriteLine($"{method.DeclaringType.Namespace} {method.DeclaringType.Name}::{method.Name} - RUN FOR YOUR LIVES");
 							continue;
 						}
 
