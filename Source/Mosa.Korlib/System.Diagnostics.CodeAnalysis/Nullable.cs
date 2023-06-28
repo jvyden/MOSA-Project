e76248714c2da59442b7069080914c810801a9db
jvyden <jvyden@jvyden.xyz>
2023-06-28 03:30:46 -0400

    good lord am i ok?

--- Source/Mosa.Korlib/System.Diagnostics.CodeAnalysis/Nullable.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) MOSA Project. Licensed under the New BSD License.
-
-namespace System.Diagnostics.CodeAnalysis;
-
-public class Nullable
-{}
---
+++ Source/Mosa.Korlib/System.Runtime.CompilerServices/Nullable.cs
@@ -0,0 +1,28 @@
+// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace System.Runtime.CompilerServices;
+
+// // [CompilerGenerated]
+// [Microsoft.CodeAnalysis.Embedded]
+// [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.GenericParameter, AllowMultiple = false, Inherited = false)]
+// internal sealed class NullableAttribute : Attribute
+// {
+// 	public readonly byte[] NullableFlags;
+//
+// 	public NullableAttribute(byte P_0)
+// 	{
+// 		byte[] array = new byte[1];
+// 		array[0] = P_0;
+// 		NullableFlags = array;
+// 	}
+//
+// 	public NullableAttribute(byte[] P_0)
+// 	{
+// 		NullableFlags = P_0;
+// 	}
+// }
+
+public class Nullable
+{
+
+}
